e52bffabe04b83251ead9696e667803c62af4706
kmd <mtisya@gmail.com>
2021-09-28 23:32:18 +0300

    This is our fourth commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
-
-        today is a good day
+        sskdhlajwkc
     }
 }
