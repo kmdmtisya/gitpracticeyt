58c076dabd69bbdfb2889f5dea3b845b81bf1896
kmd <mtisya@gmail.com>
2021-09-30 12:03:35 +0300

    Experimental Commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
-        sdjksfsjf
+        println("Local change")
+        print("Experimental")
     }
 }
